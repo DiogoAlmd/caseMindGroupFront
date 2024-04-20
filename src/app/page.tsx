2e20e72702acd16dd6159555e09f510b68fa0a95
Diogo Almeida <83725335+DiogoAlmd@users.noreply.github.com>
2024-04-20 12:19:11 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Login from "@/pages/login/login";
+
+export default function Home() {
+  return(
+    <main>
+      <Login/>
+      {/* <h1 className="text-danger">asdfasdf</h1> */}
+    </main>
+  )
+}
---
+++ src/pages/productList/productList.tsx
@@ -0,0 +1,7 @@
+export default function productList() {
+    return(
+      <main>
+        <h1>PRODUCT LIST</h1>
+      </main>
+    )
+  }
